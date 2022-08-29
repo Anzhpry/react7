1933442308865dc936844a82618cb5bf5b482c5f
Anzhelika <anzh201419@yandex.ua>
2022-08-29 17:03:44 +0300

    Homework 2

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,52 +1,47 @@
 import { useState, useEffect } from 'react';
 
 export const Form = ({ messageList, setMessageList }) => {
-    const [robotText, setRobotText] = useState();
-    const [text, setText] = useState();
-    const author = 'Name';
+  const [robotText, setRobotText] = useState();
+  const [text, setText] = useState();
+  const author = 'Name';
 
-    useEffect(() => {
-        if (messageList.length !== 0) {
-            setTimeout(() => {
-                setRobotText(`${author}, отправлено`);
-                setTimeout(() => {
-                    setRobotText();
-                }, 1500);
-            }, 1000);
-        }
-    }, [messageList]);
-    const handleText = (event) => {
-        setText(event.target.value);
+  useEffect(() => {
+    if (messageList.length !== 0) {
+      setTimeout(() => {
+        setRobotText(`${author}, отправлено`);
+        setTimeout(() => {
+          setRobotText();
+        }, 1500);
+      }, 1000);
+    }
+  }, [messageList]);
+  const handleText = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleMessages = () => {
+    const message = {
+      author: author,
+      text: text,
     };
+    messageList.push(message);
+    setMessageList([...messageList]);
+  };
 
-    const handleMessages = () => {
-        const message = {
-            author: author,
-            text: text,
-        };
-        messageList.push(message);
-        setMessageList([...messageList]);
-    };
-
-    return (
-        <>
-            <ul >
-                {messageList.map((item, idx) => (
-                    <li key={idx}>
-                        {item.author} : {item.text}
-                    </li>
-                ))}
-            </ul>
-            <div>
-                <textarea
-                    type="text"
-                    onBlur={handleText}
-                ></textarea>
-                <button onClick={handleMessages}>
-                    Отправить
-                </button>
-                <p>{robotText}</p>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <ul>
+        {messageList.map((item, idx) => (
+          <li key={idx}>
+            {item.author} : {item.text}
+          </li>
+        ))}
+      </ul>
+      <div>
+        <textarea type="text" onBlur={handleText}></textarea>
+        <button onClick={handleMessages}>Отправить</button>
+        <p>{robotText}</p>
+      </div>
+    </>
+  );
 };

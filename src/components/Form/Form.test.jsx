117f3a7d90c0679825c724b074615086522e8625
Anzhelika <anzh201419@yandex.ua>
2022-09-05 15:10:32 +0300

    неполучившиеся тесты

--- src/components/Form/Form.test.jsx
+++ src/components/Form/Form.test.jsx
@@ -3,14 +3,14 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe('Form', () => {
-    it('render component', () => {
-        render(<Form />);
-    });
+  it('render component', () => {
+    render(<Form />);
+  });
 
-    it('button is disabled', () => {
-        /*   const test = 'text'; */
-        render(<Form disabled={false} />);
+  it('button is disabled', () => {
+    /*   const test = 'text'; */
+    render(<Form disabled={false} />);
 
-        expect(screen.getByText(test)).toBeDisabled();
-    });
+    expect(screen.getByText(test)).toBeDisabled();
+  });
 });
